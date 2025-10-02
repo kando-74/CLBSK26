ea4201a3cb717736d319f4d99cd709fb972b3204
Alejandro <alejandro.martin.millan@gmail.com>
2025-10-03 01:26:45 +0200

    git commit

--- frontend/src/pages/Organization.tsx
+++ frontend/src/pages/Organization.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState, type FormEvent } from 'react'
+import { useCallback, useEffect, useMemo, useState, type ChangeEvent, type FormEvent } from 'react'
 import { Link } from 'react-router-dom'
 import {
   ArrowDownToLine,
